998b2d9a8c67c7060cd70daca8d965f9f7e8edb7
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-07-05 23:52:01 -0300

    Clase N°3: Function Scope.

---
+++ src/scope/function.ts
@@ -0,0 +1,11 @@
+const greetin = () => {
+    let userName = 'Ana';
+    console.log(userName);
+
+    if (userName === 'Ana') {
+        console.log(`Hello ${userName}`);
+    }
+};
+greetin();
+
+console.log(userName);
