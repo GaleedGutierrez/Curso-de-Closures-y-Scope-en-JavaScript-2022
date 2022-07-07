e71799ac5e325d7bffa15c704b973ec82e3b55a3
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-07-06 21:23:44 -0300

    Clase N°5: Reasignación y redeclaración.

---
+++ src/scope/re.ts
@@ -0,0 +1,29 @@
+var firstName; // Undefined
+firstName = 'Galeed';
+console.log(firstName);
+
+var lastName = 'Gutierrez'; // Declarar y asignar
+lastName = 'Leaño'; // Reasignar
+console.log(lastName);
+
+var secondName = 'Enrique'; // Declarar y asignar
+var secondName = 'Aaron'; // Reasignación y redeclaración
+console.log(secondName);
+
+// Let
+let fruit = 'Apple'; // Declarar y asignar
+fruit = 'Kiwi'; // Reasignar
+console.log(fruit);
+// let fruit = 'Banana'; // Error
+
+// Const
+const animal = 'dog'; // Declarar y asignar
+// animal = 'cat'; // Reasignar / Error
+// const animal = 'snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('🚗');
+console.log(vehicles);
+vehicles.pop();
+console.log(vehicles);
