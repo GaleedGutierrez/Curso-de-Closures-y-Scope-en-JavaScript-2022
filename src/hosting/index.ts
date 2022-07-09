a7617c6e30c5b32393c8a091282f2b3107b438c2
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-07-09 15:01:28 -0300

    Clase N°9: ¿Qué es el Hoisting?

---
+++ src/hosting/index.ts
@@ -0,0 +1,15 @@
+// console.log(nameOfDog);
+// var nameOfDog = 'Elmo';
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog () {
+    console.log(`The best dog is ${elmo}`);
+}
+// const nameOfDog = () => {
+//     console.log(`The best dog is ${elmo}`);
+// }
+
+var elmo = 'Elmo';
+
